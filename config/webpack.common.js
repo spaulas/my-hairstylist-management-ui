549648518d82ab842d6efaaf16d45b571ad783c8
Paula Santos <s.paula@ua.pt>
2022-05-30 16:26:16 +0100

    first commit

---
+++ config/webpack.common.js
@@ -0,0 +1,25 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts", ".html", ".scss"],
+  },
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationLogin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8082,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationLogin({
+      name: "management",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ManagementApp": "./src/bootstrap",
+      },
+    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
