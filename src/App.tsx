12969f7b85ce74d89c00c9166488dcae98826296
Paula Santos <s.paula@ua.pt>
2022-08-08 16:36:53 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
-import Landing from './components/Landing'
-import Pricing from './components/Pricing'
+import { Switch, Route, Router } from "react-router-dom";
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
-export default (): React.ReactElement => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path='/pricing' component={Pricing}/>
-                    <Route exact path='/' component={Landing}/>
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
-    </div>
-}
+export default ({history}: { history: any }): React.ReactElement => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/pricing" component={Pricing} />
+        <Route exact path="/" component={Landing} />
+      </Switch>
+    </Router>
+  );
+};
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,16 +1,33 @@
 import React from "react";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (nextPathname !== pathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#management-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
